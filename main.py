eba43b826dd1bebd39de024ebeb25b125338d00a
Marcel Ghisi <marcel.ghisi@gmail.com>
2023-08-08 18:40:29 +0200

    Decide function and exit add func

--- main.py
+++ main.py
@@ -8,11 +8,27 @@ a = None
 b = None
 op = None
 
+# Add function
+# a -- argument 1
+# b -- argument 2
+def add(a, b):
+    return a + b
+
 while (True):
     # get input values
-    a =  input("Enter the first argument: ")
-    op = input("Enter the operation: ")
-    b =  input("Enter the second argument: ")
+    a =  input("Enter the first numeric argument or [q] to exit: ")
+    if (str(a) == 'q'):
+        exit()
+
+    op = input("Enter the operation [+,-,*,/,q (exit)]: ")
+
+    if str(op) == 'q':
+        exit()
+
+    b =  input("Enter the second numeric argument or [q] to exit:  ")
+
+    if str(b) == 'q':
+        exit()
 
     try:
         a = int(a)
@@ -20,3 +36,16 @@ while (True):
     except ValueError:
         print("Invalid number argument...")
         op = None
+
+                    # decide function
+    if (op != None):
+        if (op == "+"):
+            print ("Sum:: ", add(a, b))
+        elif (op == "-"):
+            print("Difference:")
+        elif (op == "*"):
+            print("Product:")
+        elif (op == "/"):
+            print("Quotient:")
+        else:
+            print("Invalid operation...")
