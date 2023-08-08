8d9fcd7d2a983bb51762848e6a3f8224bce169a8
Cristian Ghisi <cristian.ghisi@gmail.com>
2023-08-08 16:18:01 +0200

    Creating main loop

---
+++ main.py
@@ -0,0 +1,16 @@
+import sys
+
+# -------------------------------------------------------- #
+# -- MAIN FUNCTIONAILTY -- DO NOT EDIT ------------------- #
+# -------------------------------------------------------- #
+
+a = None
+b = None
+op = None
+
+while (True):
+    # get input values
+    a = raw_input("Enter the first argument: ")
+    op = raw_input("Enter the operation: ")
+    b = raw_input("Enter the second argument: ")
+
